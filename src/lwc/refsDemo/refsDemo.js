0858079b1a0e84f5558e0b964e13d436d17e4445
AshB <ashbasn@gmaill.com>
2023-12-26 14:11:54 -0600

    lwc refs demo

---
+++ src/lwc/refsDemo/refsDemo.js
@@ -0,0 +1,12 @@
+import {LightningElement} from 'lwc';
+export default class RefsDemo extends LightningElement {
+  submitHandler(){
+    const nameVal = this.refs.nameRef.value;
+    const ageVal = this.refs.ageRef.value;
+    console.log(nameVal);
+    console.log(ageVal);
+
+    this.refs.responseRef.innerHTML = `Submitted Name is : ${nameVal} and Age is : ${ageVal}`;
+
+  }
+}

9ec979a2f55371f5f25a170bf92b46b5d9d5d3d5
AshB <ashbasn@gmaill.com>
2023-12-30 14:36:21 -0600

    working refreshApex

--- src/lwc/refreshLwcDemo/refreshLwcDemo.js
+++ src/lwc/refreshLwcDemo/refreshLwcDemo.js
@@ -1,64 +1,64 @@
-import { LightningElement,wire } from 'lwc';
+import { LightningElement, wire } from 'lwc';
 import getContactList from '@salesforce/apex/refreshContactController.getContactList';
-import { updateRecord } from 'lightning/uiRecordApi';
-import{ShowToastEvent} from 'lightning/platformShowToastEvent';
-import {refreshApex} from '@salesforce/apex'; 
+import {updateRecord} from "lightning/uiRecordApi";
+import {ShowToastEvent} from "lightning/platformShowToastEvent";
+import {refreshApex} from "@salesforce/apex";
+
 const columns = [
-    { label: 'First Name', fieldName: 'FirstName',editable: true },
-    { label: 'Last Name', fieldName: 'LastName', editable: true },
-    { label: 'Email', fieldName: 'Email', type: 'email' }
-];
+    {label:'First Name', fieldName:'FirstName', editable:true},
+    {label:'Last Name', fieldName:'LastName', editable:true},
+    {label:'Email', fieldName:'Email', type:'email'}
+]
 export default class RefreshLwcDemo extends LightningElement {
     columns = columns;
-    contacts=[];
-    draftValues=[];
+    draftValues = [];
+
+    // if using a function, need to pass contactResult into refreshApex
+    // @wire(getContactList)
+    // contact (result) {
+    //     contactResult = result;
+    //     const {data, value} = result;
+    // }
+
     @wire(getContactList)
-    contact; 
-    // get isContactAvailable(){
-    //     console.log(JSON.stringify(this.contact))
-    //     return this.contact && this.contact.data && this.contact.data.length>0? 'Yes':'No'
+    contact
+    get isContactAvailable () {
+        console.log('contact:', JSON.stringify(this.contact));
+        return this.contact && this.contact.data && this.contact.data.length>0? 'Yes' : 'No';
+    }
 
-    // }
-    // listViewHandler({data, error}){
-    //     if(data){
-    //         console.log(data)
-    //         this.contacts = data.records.records.map(item=>{
-    //             return {
-    //                 "Id": this.getValue(item, 'Id'),
-    //                 "Name": this.getValue(item, 'Name'),
-    //                 "Title": this.getValue(item, 'Title'),
-    //                 "Phone": this.getValue(item, 'Phone'),
-    //                 "Email": this.getValue(item, 'Email')
-    //             }
-    //         })
-    //     }
-    //     if(error){
-    //         console.error(error)
-    //     }
-    // }  
-    
-    // getValue(data, field){
-    //     return data.fields[field].value
-    // }
+    handleSave(event) {
+        console.log('draftValues:', event.detail.draftValues);
+        // updateRecord updates record one at a time, so let's make an array
+        // slice to make a new copy of the object
+        // loop with map that takes each draft value and make a new object with Object.assign
+        const recordInputs = event.detail.draftValues.slice().map(draft=>{
+            const fields = Object.assign({}, draft)
+            return {fields}
+        })
+        console.log('recordInputs: ', JSON.stringify(recordInputs));
+        // call update method
+        // since this is asynchronous, we get promise and we store all the promises.
+        // it returns an array of promises
+        const promises = recordInputs.map(recordInput => updateRecord((recordInput)))
+        // when all the promise are resolved, this will run-
+        Promise.all(promises).then(result=>{
+            this.showToastMsg('Success!', 'Contacts updated', 'success');
+            this.draftValues = []; // clearing this otherwise the values show yellow in UI
+            return refreshApex(this.contact) // need to pass reference, the first property that holds the data
+        }).catch(error=>{
+            this.showToastMsg('Error!', error.body.message, 'error');
+        })
 
-    handleSave(event){
-        console.log('These are the draft values:',JSON.stringify(event.detail.draftValues))
-        const recordInputs=event.detail.draftValues
-        
-        console.log('Thes are the recordInputs:',recordInputs)
+    }
 
-        updateRecord(recordInputs)
-        this.draftValues=[]
-        return refreshApex(this.contact)
-
-        // const promises = recordInputs.map(recordInput=>updateRecord(recordInput))
-        // Promise.all(promises).then(()=>{
-        //     console.log('COntact updated Successfully')
-        //     this.draftValues=[]
-        //     return refreshApex(this.contact)
-        // }).catch(error=>{
-        //     console.error("Error updating the record", error)
-        // })
-        
+    showToastMsg(title, message, variant){
+        const evt = new ShowToastEvent({
+            title,
+            message,
+            variant
+            }
+        )
+        this.dispatchEvent(evt);
     }
 }

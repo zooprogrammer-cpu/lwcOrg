c8250c4b2c958ddaba50d96c2faafb12993ad8a1
AshB <ashbasn@gmaill.com>
2024-06-03 16:16:41 -0500

    WorkspaceAPI - setTabLabel

---
+++ src/lwc/setLabelOfTab/setLabelOfTab.js
@@ -0,0 +1,16 @@
+
+
+import {LightningElement, wire} from 'lwc';
+import {IsConsoleNavigation, getFocusedTabInfo, setTabLabel} from "lightning/platformWorkspaceApi";
+
+export default class SetLabelOfTab extends LightningElement {
+  @wire(IsConsoleNavigation)
+  isConsoleApp
+
+  async setLabelHandler() {
+    if (this.isConsoleApp) {
+      const {tabId} = await getFocusedTabInfo()
+      await setTabLabel(tabId, "Hurray!!")
+    }
+  }
+}

761245a12a07c9b0f220a5f06ae8513b5227a176
AshB <ashbasn@gmaill.com>
2024-06-27 08:36:05 -0500

    LWC for Flow Final - Navigate Screen Flows

--- src/lwc/lwcForFlow/lwcForFlow.js
+++ src/lwc/lwcForFlow/lwcForFlow.js
@@ -6,9 +6,10 @@ import {LightningElement, api} from 'lwc';
 import {FlowNavigationBackEvent, FlowNavigationNextEvent} from "lightning/flowSupport";
 
 export default class LwcForFlow extends LightningElement {
-  @api availableActions = [];
+  @api availableActions = []; // Gives us the ability to know what actions are available in the flow
 
   handleNext() {
+    // If we are already in the last screen, we don't want to do anything
     if(this.availableActions.find((action)=> action === "NEXT")) {
       const navigateNextEvent = new FlowNavigationNextEvent();
       this.dispatchEvent(navigateNextEvent);
@@ -16,9 +17,12 @@ export default class LwcForFlow extends LightningElement {
   }
 
   handleBack() {
-    const navigateBackEvent = new FlowNavigationBackEvent();
-    this.dispatchEvent(navigateBackEvent);
-  }
+    // If we are already in the first screen, we don't want to do anything
+    if (this.availableActions.find((action)=> action === "BACK")) {
+      const navigateBackEvent = new FlowNavigationBackEvent();
+      this.dispatchEvent(navigateBackEvent);
+    }
 
+  }
 
 }

7842541d6943721d95e094af62ee77af18e3287b
ashbas <ashbasn@gmail.com>
2025-07-25 12:55:58 -0500

    Retain Previous values for dates in LWC embedded in Flow

--- src/lwc/retainPreviousValues/retainPreviousValues.js
+++ src/lwc/retainPreviousValues/retainPreviousValues.js
@@ -4,26 +4,45 @@ import {FlowAttributeChangeEvent, FlowNavigationNextEvent} from 'lightning/flowS
 
 export default class RetainPreviousValues extends LightningElement {
   @track _txtBoxVal = '';
+  @track _selectedDate;
   @api availableActions = [];
-   @api selectedDate;
 
   @api
   get txtBoxVal(){
     return this._txtBoxVal;
   }
 
+  @api
+  get selectedDate() {
+    return this._selectedDate;
+  }
+
   set txtBoxVal(val){
     this._txtBoxVal = val;
+  }
+
+  set selectedDate(val){
+    this._selectedDate = val;
   }
 
   handleChange(event) {
     this._txtBoxVal = event.target.value;
   }
 
+  handleDateChange(event) {
+    this._selectedDate = event.target.value;
+  }
+
   //Change attribute on Flow
   handleClick(event) {
     const attributeChangeEvent = new FlowAttributeChangeEvent('txtBoxVal', this._txtBoxVal);
     this.dispatchEvent(attributeChangeEvent);
+  }
+
+  handleClickDate(event) {
+    const attributeChangeEvent = new FlowAttributeChangeEvent('selectedDate', this._selectedDate);
+    this.dispatchEvent(attributeChangeEvent);
+
   }
 
   //Hook to Flow's Validation engine
